d3559da737a2b0f111cd1fa84cd3c1ebd75f5101
Clevin M.Wilson <clevinmwilson@gmail.com>
2022-08-05 12:49:45 +0530

    list product in iphone page function added

--- src/Components/Iphone/Iphone.js
+++ src/Components/Iphone/Iphone.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import styled from 'styled-components';
 import Ribborn from '../Ribborn/Ribborn';
+import {db} from '../Firebase/Firebase';
+import BeatLoader from "react-spinners/BeatLoader";
 
 function Iphone() {
+    const [phones,setphones]=useState([]);
+
+    useEffect(() => {
+        db.collection('products').get().then((snapshort) => {
+            const allPhones = snapshort.docs.map((product) => {
+                if (product.data().category == "phone"){
+                    console.log(product.data());
+                    return (
+                        product.data()
+                    )
+                }
+            })
+             setphones(allPhones)
+        })
+    },[])
+    
     return (
         <Container>
             <Ribborn />
@@ -56,67 +74,77 @@ function Iphone() {
                             <div class="heading text-center ">
                                 <h2 class="typography-headline">Which iPhone is right for you?</h2>
                                 <div style={{ fontSize: "19px" }} class="m-4 banner-links text-center  ">
-                                    <a href="">Compare all iPhone models <i style={{ fontSize: "10px" }}
-                                        class="banner-links-icons font-weight-normal fas fa-chevron-right"></i></a>
+                                    <a href="">Compare all iPhone models   <i style={{ fontSize: "13px" }} class="mr-2 fa-solid fa-angle-right"></i></a>
+                                        
                                 </div>
                             </div>
 
                         </div>
                     </div>
                     <div id="iphones" class="row m-1">
-                        <div class="col-md-3 col-6 p-0 ">
-                            <div class="product-image-outer text-center">
-                                <img class="product-image image-compare-iphone-12-pro" src="/images/compare_iphone_12_pro__gmn3556ju3am_large.jpg" alt="Product image" />
-                                <div class="device-title ">
-                                    <h3 class="device-name">iPhone12Pro</h3>
-                                    <p class="device-price-spec  ">From ₹119900*</p>
-                                    <img class="device-colors" src="images/swatches_iphone_12_pro__for4g8pne0ey_large.jpg"
-                                        alt="" />
-                                    <div class="product-buy">
-                                        <a href="" class="device-buy-btn  btn btn-primary">Buy</a>
-                                    </div>
-                                </div>
-                                <div class="mt-5 device-details">
-                                    <h4 style={{ fontSize: "17px" }} class="device-details-head">15.5 cm (6.1″) or 17 cm (6.7″)</h4>
-                                    <p class="device-details-text">all-screen OLED display1</p>
-                                </div>
-                                <div class="device-memory mt-5">
-                                    <img src="/images/icon_5g__ew2qs88wie4i_large.png" alt="" />
-                                    <p class="mt-2 device-details-text">5G capable</p>
-                                </div>
-                                <div class="device-chip mt-5 ">
-                                    <img src="/images/icon_a14__e0ozekurulm6_large.png" alt="" />
-                                    <p class="mt-2 device-details-text">A14 Bionic chip</p>
-                                    <p class="device-subtext">Fastest chip in a smartphone</p>
-                                </div>
-                                <div class="mt-5 device-details">
-                                    <h4 class="device-details-head">2TB</h4>
-                                    <p class="device-details-text">Maximum configurable storage2</p>
-                                </div>
-                                <div class="device-memory mt-5">
-                                    <img src="/images/icon_triple_camera__e6vgz42wpwom_large.png" alt="" />
-                                    <p class="mt-2 device-details-text">Pro camera system</p>
-                                    <p class="device-subtext">Ultra Wide, Wide, Telephoto
+                       {
+                        phones[0]?
+                                phones.map((phone) => {
+                                    return (
+                                        <div class="col-md-3 m-3 col-6 p-0 ">
+                                            <div class="product-image-outer text-center">
+                                                <img class="product-image image-compare-iphone-12-pro" src={phone.productImage} />
+                                                <div class="device-title ">
+                                                    <h3 class="device-name">{phone.productName}</h3>
+                                                    <p class="device-price-spec  ">From ₹{phone.productPrice}*</p>
+                                                    <img class="device-colors" src={phone.allColorImage}
+                                                        alt="" />
+                                                    <div class="product-buy">
+                                                        <a href="" class="device-buy-btn  btn btn-primary">Buy</a>
+                                                    </div>
+                                                </div>
+                                                <div class="mt-5 device-details">
+                                                    <h4 style={{ fontSize: "17px" }} class="device-details-head">{phone.display}</h4>
+                                                    <p class="device-details-text">all-screen OLED display1</p>
+                                                </div>
+                                                <div class="device-memory mt-5">
+                                                    <img src="/images/icon_5g__ew2qs88wie4i_large.png" alt="" />
+                                                    <p class="mt-2 device-details-text">5G capable</p>
+                                                </div>
+                                                <div class="device-chip mt-5 ">
+                                                    <img src={phone.processorImage} alt="" />
+                                                    <p class="mt-2 device-details-text">{phone.processor}</p>
+                                                    <p class="device-subtext">Fastest chip in a smartphone</p>
+                                                </div>
+                                                <div class="mt-5 device-details">
+                                                    <h4 class="device-details-head">{phone.storageOptions[0].storage}</h4>
+                                                    <p class="device-details-text">Maximum configurable storage2</p>
+                                                </div>
+                                                <div class="device-memory mt-5">
+                                                    <img src="/images/icon_triple_camera__e6vgz42wpwom_large.png" alt="" />
+                                                    <p class="mt-2 device-details-text">Pro camera system</p>
+                                                    <p class="device-subtext">Ultra Wide, Wide, Telephoto
 
-                                    </p>
-                                </div>
-                                <div class="device-chip mt-5 ">
-                                    <img src="/images/icon_lidar__f6bhejsvbdqy_large.png" alt="" />
-                                    <p class="mt-2 device-details-text">LiDAR Scanner</p>
-                                    <p class="device-subtext">For Night mode portraits and next‑level AR </p>
-                                </div>
-                                <div class="device-memory mt-5">
-                                    <img src="/images/icon_magsafe__ibh1kzdigzqm_large.png" alt="" />
-                                    <p class="mt-2 device-details-text">Compatible with</p>
-                                    <p class="device-subtext">MagSafe accessories </p>
-                                </div>
-                                <div class="product-link">
-                                    <a style={{ fontSize: "14px", letterSpacing: "0.02em" }} href="iphone-12.html"> Learn more <span
-                                        style={{ fontSize: "9px" }} class="fas fa-chevron-right"></span></a>
-                                </div>
+                                                    </p>
+                                                </div>
+                                                <div class="device-chip mt-5 ">
+                                                    <img src="/images/icon_lidar__f6bhejsvbdqy_large.png" alt="" />
+                                                    <p class="mt-2 device-details-text">LiDAR Scanner</p>
+                                                    <p class="device-subtext">For Night mode portraits and next‑level AR </p>
+                                                </div>
+                                                <div class="device-memory mt-5">
+                                                    <img src="/images/icon_magsafe__ibh1kzdigzqm_large.png" alt="" />
+                                                    <p class="mt-2 device-details-text">Compatible with</p>
+                                                    <p class="device-subtext">MagSafe accessories </p>
+                                                </div>
+                                                <div class="product-link">
+                                                    <a style={{ fontSize: "14px", letterSpacing: "0.02em" }} href="iphone-12.html"> Learn more <span
+                                                        style={{ fontSize: "9px" }} class="fas fa-chevron-right"></span></a>
+                                                </div>
 
-                            </div>
-                        </div>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                                :
+                                <BeatLoader style={{justifyContent: "center"}} color={"#0066cc"} loading={phones} size={17} />
+                       }
+                        
                     </div>
                 </div>
             </section>
